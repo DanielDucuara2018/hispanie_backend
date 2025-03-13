fc518a6dc9d9e76a8a99308e6358b5130a9e9ee7
gyelan <38592946+gyelan@users.noreply.github.com>
2025-03-13 10:07:53 +0100

    Fixed some idempotency cases and added rollback for db errors (#15)
    * Fixed some idempotency cases and added rollback for db errors
    
    * Added remove duplicates in handle_update_resources
    
    * Fixed up delete_duplicates

--- hispanie/action/event.py
+++ hispanie/action/event.py
@@ -2,7 +2,12 @@ import logging
 
 from ..model import Activity, Event, File, Tag
 from ..schema import EventCreateRequest, EventUpdateRequest
-from ..utils import ensure_user_owns_resource, handle_update_files, handle_update_resources
+from ..utils import (
+    delete_duplicates,
+    ensure_user_owns_resource,
+    handle_update_files,
+    handle_update_resources,
+)
 from .account import read as read_accounts
 from .tag import read as read_tags
 
@@ -14,7 +19,7 @@ def create(event_data: EventCreateRequest, account_id: str) -> Event:
     data = event_data.model_dump()
     logger.info("Adding new event: %s", data)
     # Format and check extra models
-    activities = [Activity(**act) for act in data.pop("activities")]
+    activities = [Activity(**act) for act in delete_duplicates(data.pop("activities"), "name")]
     files = [File(**file).create() for file in data.pop("files")]
     tags = read_tags(id=[tag["id"] for tag in data.pop("tags")])
     event = Event(
@@ -43,12 +48,17 @@ def update(event_id: str, account_id: str, event_data: EventUpdateRequest) -> Ev
     data = event_data.model_dump(exclude_none=True)
     logger.info("Updating event: %s with %s", event_id, data)
     # Format and check tags
-    if tags := data.pop("tags", []):
-        data["tags"] = [Tag.get(id=t["id"]) for t in tags]
+    if activities := data.pop("activities", []):
+        data["activities"] = handle_update_resources(
+            activities,
+            event.activities,
+            Activity,
+            remove_duplicates=True,
+        )
     if files := data.pop("files", []):
         data["files"] = handle_update_files(files, File)
-    if activities := data.pop("activities", []):
-        data["activities"] = handle_update_resources(activities, event.activities, Activity)
+    if tags := data.pop("tags", []):
+        data["tags"] = [Tag.get(id=t["id"]) for t in tags]
     result = event.update(**data)
     logger.info("Updated event: %s", event_id)
     return result
