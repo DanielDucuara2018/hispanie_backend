b81c1fe072e7dfed4cbb5b91b13d782394828acf
gyelan <sagudelor@unal.edu.co>
2025-03-17 16:44:55 +0100

    Fixed up typing alembic script

--- alembic/versions/c5614ae0da1b_0001_initial_revision.py
+++ alembic/versions/c5614ae0da1b_0001_initial_revision.py
@@ -1,22 +1,22 @@
 """0001 - Initial revision.
 
-Revision ID: 7de986a078ba
+Revision ID: c5614ae0da1b
 Revises:
-Create Date: 2025-03-15 20:00:29.845730
+Create Date: 2025-03-17 15:33:22.201163
 
 """
 
-from typing import Sequence, Union
+from typing import Sequence
 
 import sqlalchemy as sa  # noqa: F401
 
 from alembic import op  # noqa: F401
 
 # revision identifiers, used by Alembic.
-revision: str = "7de986a078ba"
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "c5614ae0da1b"
+down_revision: str | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
